e4fe246f7a678adeb585420c6621a618ba7376f8
vskokan <victoriaskokan@gmail.com>
2021-01-28 19:49:12 +0300

    Fix axios config

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+axios.defaults.withCredentials = true
 
 export default axios.create({
   baseURL: "http://localhost:3000/api", //для локальной сети попменять на 192.168.0.102
   headers: {
     //"Content-type": "application/json",
     "Content-type": "multipart/form-data",
-    withCredentials: true,
-  }
+    "Access-Control-Allow-Origin": "http://localhost:3000/api"
+    // withCredentials: true,
+  },
+  
+  
 });
 
