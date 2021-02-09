534bae48dd1b9b7216f45ea2982da28d0c40b799
vskokan <victoriaskokan@gmail.com>
2021-02-09 23:52:34 +0300

    Fix review card position

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,7 +8,7 @@
                 :balloon-template="balloonTemplate"
             ></ymap-marker>
         </yandex-map> -->
-        <yandex-map :coords="coords" :zoom="12" @click="onClick">
+        <yandex-map :coords="coords" :zoom="10" @click="onClick">
             <!-- <ymap-marker v-click-outside="hide" @click="toggle(review)" v-for="review of reviews" :key="review" :icon="markerIcon"
                 marker-id="review.id" 
                 :coords= "[`${review.latitude}`, `${review.longitude}`]"
@@ -20,12 +20,12 @@
                   marker-id="0" 
                   v-if="showForm"
                 />
-            <ymap-marker v-for="review in allReviews" :key="review.id" marker-id="review.id" :coords="[`${review.latitude}`, `${review.longitude}`]" @click="chooseReview(review)"  :options="markerIcon"/>
-
+            <ymap-marker  v-for="review in allReviews" :key="review.id" marker-id="review.id" :coords="[`${review.latitude}`, `${review.longitude}`]" @click="chooseReview(review); move(review.latitude, review.longitude)"  :options="markerIcon"/>
+             <ReviewCard  class="cardForm" v-if="showCard" v-bind:review="currentReview"/>
         </yandex-map>
         <transition name="fade">
-        <ReviewCard class="cardForm" v-if="showCard" v-bind:review="currentReview"/>
-        <AddReview class="cardForm" v-if="showForm" v-bind:latitude="coords[0]" v-bind:longitude="coords[1]"/>
+       
+        <AddReview class="addForm" v-if="showForm" v-bind:latitude="coords[0]" v-bind:longitude="coords[1]"/>
         </transition>
     </div>
 </template>
@@ -33,9 +33,9 @@
 <script>
 
 import { mapGetters, mapActions, mapMutations } from "vuex";
-import ReviewCard from '@/components/Reviews/ReviewCard'
+// import ReviewCard from '@/components/Reviews/AdminPanel/ReviewCard'
 import AddReview from '@/components/Reviews/AddReview'
-// import ReviewCard from '@/components/ReviewCard'
+import ReviewCard from '@/components/Reviews/ReviewCard'
 // import ClickOutside from 'vue-click-outside'
 
 export default {
@@ -44,6 +44,7 @@ export default {
   data: () => { 
     return {
     coords: [54.62880826189078,39.75270322600586],
+    zoom: 12,
     //opened: false,
     markerIcon: {
       // layout: 'default#image',
@@ -76,11 +77,19 @@ export default {
             // alert(this.showForm)
         
     },
+    move(lat, long) {
+      this.coords = [lat, long]
+    },
     chooseReview(review) {
-            
             if (!this.showCard) {
+              // this.changeCardView()
                 this.currentReview = review
                 this.changeCardView()
+            } else {
+              // this.changeCardView()
+              // alert('Закрыли и откроем')
+              this.currentReview = review
+                // this.changeCardView()
             }
             // this.changeCardView()
     },
@@ -114,12 +123,15 @@ export default {
 <style scoped>
 
     .ymap-container {
-      height: 820px;
+      height: 830px;
       width:  100%;
     }
 
     .map {
+      position: relative;
+      top: 1px;
       width: 100%;
+      z-index:0;
       min-height: calc(100vh - 320px)
     }
 
@@ -129,12 +141,17 @@ export default {
     }
 
        .addForm, .cardForm, .editForm {
-       position: absolute;
+       position: relative;
        margin: auto;
        margin-top: -650px;
-       margin-left: 700px;
+       margin-left: 500px;
        z-index: 1;
    }
+
+      .cardForm {
+         margin-top: -620px;
+        margin-left: 1000px;
+      }
 
        .fade-enter-active, .fade-leave-active {
         transition: all 0.3s;
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,25 +1,5 @@
 <template>
     <div class="mapContainer">
-       <!-- <div class="mapDescription">
-           <p class="heading">Карта рыболовных мест</p>
-           <div class="icons"><i class="fas fa-map-marker-alt"></i><i class="fas fa-chart-pie"></i><i class="fas fa-percentage"></i></div>
-           <div class="text">
-
-            <p>
-               Зарегистрируйтесь, чтобы иметь возможность оставлять свои отзывы простым кликом мыши по карте. 
-            </p>
-            <p>
-                Мы анализируем информацию из отзывов, чтобы собрать реальную статистику уловов.
-            </p>
-            <p>
-                Мы поощряем внесенный пользователями вклад бонусами от наших партнеров. Подробннее об этом читайте здесь.
-            </p>
-           </div>
-           
-               
-           
-       </div> -->
-       <!-- <Map v-bind:reviews="this.reviews"></Map> -->
        <Map></Map>
     </div>
 </template>
@@ -33,19 +13,6 @@ export default {
     components: {
         Map
     },
-    // data() {
-    //     return {
-    //         reviews: [
-    //         ],
-    //     }
-    // },
-    // mounted() {
-    //     fetch('http://localhost:3000/api/reviews')
-    //     .then(response => response.json())
-    //     .then(json => {
-    //             this.reviews = json
-    //     })
-    // }    
 }
 
 </script>
@@ -57,15 +24,7 @@ export default {
   /* div {
       min-height: calc(100vh - 180px)
   } */
-    .mapDescription {
-        margin-top: 5px;
-        margin-bottom: 5px;
-        z-index: -1;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
+
 
      p  {
         margin-top: 5px;
@@ -84,5 +43,9 @@ export default {
         font-size: 50px;
         margin-left: 30px;
         margin-right: 30px;
+    }
+
+    .mapContainer {
+        height: 600px
     }
 </style>
